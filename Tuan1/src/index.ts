b3b8232f9445a3c5423429d2552f4e2752f450d1
Lê Nguyễn Phi Trường <phitruong20192020@gmail.com>
2025-08-23 14:40:12 +0700

    Bai 8

--- Tuan1/src/index.ts
+++ Tuan1/src/index.ts
@@ -1,22 +1,30 @@
-// import { Person } from "./person";
-// const Truong = new Person("Truong", 23);
-// Truong.display();
+//Bai 8
+import { Product } from "./product";
 
-import { User } from "./user";
+const products: Product[] = [
+    new Product("Book", 50),
+    new Product("Phone", 200),
+    new Product("Pen", 20),
+    new Product("Laptop", 1500),
+    new Product("Bag", 80)
+];
 
-// import { Student } from "./student";
-// const Truong1 = new Student("Hien", 21, 10);
-// Truong1.display();
+const loc = products.filter(product => product.price > 100);
+console.log(loc);
 
-// import { Car} from "./car";
-// const xeTo = new Car("Toyota", 12321, 2024);
-// xeTo.display();
+//Bai 7
+// import { User } from "./user";
+// const nguoi = new User("Truong");
+// nguoi.display();
+// nguoi.name = "Hien";
+// nguoi.display();
 
-// import { Rectangle } from "./rectangle";
-// const hinhTamGiac = new Rectangle(21, 23);
-// console.log(hinhTamGiac.calculate_area());
-// console.log(hinhTamGiac.calculate_perimeter());
+//Bai 6
+// import { Book } from "./book";
+// const sach = new Book("tat den", "ngo tat to", 1970)
+// sach.display();
 
+//Bai 5
 // import { BankAccount } from "./bankAccount";
 // const TruongGui = new BankAccount(2);
 // TruongGui.display();
@@ -25,11 +33,30 @@ import { User } from "./user";
 // TruongGui.withdraw(1);
 // TruongGui.display();
 
-// import { Book } from "./book";
-// const sach = new Book("tat den", "ngo tat to", 1970)
-// sach.display();
+//Bai 4
+// import { Rectangle } from "./rectangle";
+// const hinhTamGiac = new Rectangle(21, 23);
+// console.log(hinhTamGiac.calculate_area());
+// console.log(hinhTamGiac.calculate_perimeter());
 
-const nguoi = new User("Truong");
-nguoi.display();
-nguoi.name = "Hien";
-nguoi.display();
+//Bai 3
+// import { Car} from "./car";
+// const xeTo = new Car("Toyota", 12321, 2024);
+// xeTo.display();
+
+//Bai 2
+// import { Student } from "./student";
+// const Truong1 = new Student("Hien", 21, 10);
+// Truong1.display();
+
+//Bai 1
+// import { Person } from "./person";
+// const Truong = new Person("Truong", 23);
+// Truong.display();
+
+
+
+
+
+
+
