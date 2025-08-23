29e867ea63c07d82cfee5fc9b2413535f9faf5bf
Lê Nguyễn Phi Trường <phitruong20192020@gmail.com>
2025-08-23 14:29:53 +0700

    Bai 6

---
+++ Tuan1/src/book.ts
@@ -0,0 +1,16 @@
+// 6. Create a class Book with attributes title, author, year.
+export class Book {
+    title: string;
+    author: string;
+    year: number;
+
+    constructor(title: string, author: string, year: number) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    display(): void {
+        console.log(`Title: ${this.title}, Author: ${this.author}, Year: ${this.year}`);
+    }
+}
--- Tuan1/src/index.ts
+++ Tuan1/src/index.ts
@@ -3,21 +3,24 @@
 // import { Car} from "./car";
 // import { Rectangle } from "./rectangle";
 import { BankAccount } from "./bankAccount";
+import { Book } from "./book";
 
 
 // const Truong = new Person("Truong", 23);
 // const Truong1 = new Student("Hien", 21, 10);
 // const xeTo = new Car("Toyota", 12321, 2024);
 // const hinhTamGiac = new Rectangle(21, 23);
-const TruongGui = new BankAccount(2);
+// const TruongGui = new BankAccount(2);
+const sach = new Book("tat den", "ngo tat to", 1970)
 
 // Truong.display();
 // Truong1.display();
 // xeTo.display();
 // console.log(hinhTamGiac.calculate_area());
 // console.log(hinhTamGiac.calculate_perimeter());
-TruongGui.display();
-TruongGui.deposit(2);
-TruongGui.display();
-TruongGui.withdraw(1);
-TruongGui.display();
+// TruongGui.display();
+// TruongGui.deposit(2);
+// TruongGui.display();
+// TruongGui.withdraw(1);
+// TruongGui.display();
+sach.display();
