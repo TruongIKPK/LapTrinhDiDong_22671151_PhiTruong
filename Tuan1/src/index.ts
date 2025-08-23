d847a5653d66b1ac31594269a7b6bf9949fcd4e7
Lê Nguyễn Phi Trường <phitruong20192020@gmail.com>
2025-08-23 14:20:49 +0700

    Bai tap 4

--- Tuan1/src/index.ts
+++ Tuan1/src/index.ts
@@ -1,9 +1,17 @@
-import { Person } from "./person";
-import { Student } from "./student";
-import { Car} from "./car";
-const Truong = new Person("Truong", 23);
-const Truong1 = new Student("Hien", 21, 10);
-const xeTo = new Car("Toyota", 12321, 2024);
-Truong.display();
-Truong1.display();
-xeTo.display();
+// import { Person } from "./person";
+// import { Student } from "./student";
+// import { Car} from "./car";
+import { Rectangle } from "./rectangle";
+
+
+
+// const Truong = new Person("Truong", 23);
+// const Truong1 = new Student("Hien", 21, 10);
+// const xeTo = new Car("Toyota", 12321, 2024);
+const hinhTamGiac = new Rectangle(21, 23);
+
+// Truong.display();
+// Truong1.display();
+// xeTo.display();
+console.log(hinhTamGiac.calculate_area());
+console.log(hinhTamGiac.calculate_perimeter());
