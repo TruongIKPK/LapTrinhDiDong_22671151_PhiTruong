d4f1461296a10ee095a366c469e0d2c5191e1c4c
Lê Nguyễn Phi Trường <phitruong20192020@gmail.com>
2025-08-23 14:35:18 +0700

    Bai 7

--- Tuan1/src/index.ts
+++ Tuan1/src/index.ts
@@ -1,26 +1,35 @@
 // import { Person } from "./person";
-// import { Student } from "./student";
-// import { Car} from "./car";
-// import { Rectangle } from "./rectangle";
-import { BankAccount } from "./bankAccount";
-import { Book } from "./book";
-
-
 // const Truong = new Person("Truong", 23);
-// const Truong1 = new Student("Hien", 21, 10);
-// const xeTo = new Car("Toyota", 12321, 2024);
-// const hinhTamGiac = new Rectangle(21, 23);
-// const TruongGui = new BankAccount(2);
-const sach = new Book("tat den", "ngo tat to", 1970)
-
 // Truong.display();
+
+import { User } from "./user";
+
+// import { Student } from "./student";
+// const Truong1 = new Student("Hien", 21, 10);
 // Truong1.display();
+
+// import { Car} from "./car";
+// const xeTo = new Car("Toyota", 12321, 2024);
 // xeTo.display();
+
+// import { Rectangle } from "./rectangle";
+// const hinhTamGiac = new Rectangle(21, 23);
 // console.log(hinhTamGiac.calculate_area());
 // console.log(hinhTamGiac.calculate_perimeter());
+
+// import { BankAccount } from "./bankAccount";
+// const TruongGui = new BankAccount(2);
 // TruongGui.display();
 // TruongGui.deposit(2);
 // TruongGui.display();
 // TruongGui.withdraw(1);
 // TruongGui.display();
-sach.display();
+
+// import { Book } from "./book";
+// const sach = new Book("tat den", "ngo tat to", 1970)
+// sach.display();
+
+const nguoi = new User("Truong");
+nguoi.display();
+nguoi.name = "Hien";
+nguoi.display();
