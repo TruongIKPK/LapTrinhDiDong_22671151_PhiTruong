c9028dbe983e334256821ea4c46addf6ff603728
Lê Nguyễn Phi Trường <phitruong20192020@gmail.com>
2025-09-06 14:12:10 +0700

    Bai 3

--- Tuan3/app/forget_password.tsx
+++ Tuan3/app/forget_password.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { 
   View, 
   Text, 
@@ -7,21 +7,35 @@ import {
   SafeAreaView,
   StatusBar,
   Image,
-  Animated,
-  Easing
+  TextInput
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Forget_Password(){
-    return(
-        <SafeAreaView>
-             <LinearGradient
-                    colors={['#daf1f1ff', '#63c4edff']} 
-                    locations={[0.8, 1]} 
-                    style={StyleSheet.absoluteFill}
+export default function Forget_Password() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar barStyle='dark-content'/>
+            <LinearGradient
+                colors={['#daf1f1ff', '#63c4edff']} 
+                locations={[0.8, 1]} 
+                style={styles.gradient}
             />
             <View style={styles.background}>
-                <Text>Hello</Text>
+                <Image source={require('../assets/images/Group.png')}
+                    resizeMode="contain"
+                />
+                <Text style={styles.title}>FORGET</Text>
+                <Text style={styles.title}>PASSWORD</Text>
+                <Text style={styles.subtitle}>Provide your account’s email for which you want to reset your password</Text>
+                 <View style={styles.inputContainer}>
+                    <Ionicons name="mail-outline" size={24} color="#666" style={styles.inputIcon} />
+                    <TextInput style={styles.input}>Email</TextInput>
+                 </View>
+                
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>NEXT</Text>
+                </TouchableOpacity>
             </View>
         </SafeAreaView>
     )
@@ -31,32 +45,24 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    gradient: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+    },
     background: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-    content: {
-        alignItems: 'center',
-        paddingHorizontal: 30,
-    },
-    imageContainer: {
-        marginBottom: 30,
-    },
-    image: {
-        width: 140,
-        height: 140,
-        marginBottom: 20,
-
-    },
     title: {
         textAlign: 'center',
         fontWeight: '800',
         fontSize: 30,
+        color: 'black', 
         marginBottom: 5,
-        textShadowColor: 'rgba(0, 0, 0, 0.5)',
-        textShadowOffset: { width: 1, height: 1 },
-        textShadowRadius: 3,
     },
     subtitle: {
         fontSize: 16,
@@ -68,36 +74,36 @@ const styles = StyleSheet.create({
         textShadowOffset: { width: 0.5, height: 0.5 },
         textShadowRadius: 2,
     },
-    buttonContainer: {
+     inputContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-        marginTop: 30,
-        gap: 20,
+        alignItems: 'center',
+        backgroundColor: '#c7c6c6ff',
+        paddingHorizontal: 20,
+        paddingVertical: 8,
+        width: '80%',
+        marginBottom: 30,
+        shadowColor: "#000",
     },
-    button: {
-        backgroundColor: 'rgba(252, 252, 68, 0.9)',
-        paddingVertical: 15,
-        paddingHorizontal: 25,
-        borderRadius: 15,
+    inputIcon: {
+        marginRight: 14,
+    },
+    input: {
         flex: 1,
+        fontSize: 15,
+        color: '#333',
+    },
+     button: {
+        backgroundColor: 'rgba(252, 252, 68, 0.9)',
+        width:'80%',
+        paddingTop: 15,
+        paddingBottom: 15,
         alignItems: 'center',
         shadowColor: "#000",
         shadowRadius: 4.65,
         elevation: 8,
     },
-    buttonText: {
-        fontWeight: '800',
-        color: 'black',
+    buttonText:{
         fontSize: 16,
-    },
-    footer:{
-        alignItems: 'center',
-        marginBottom: 80,
-        fontWeight: '900'
-    },
-    footer_txt:{
-        fontSize: 18, 
-        fontWeight: '700'
+        fontWeight: 900
     }
 });
